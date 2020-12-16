153069c9a84bd359f8ba10d33c93e77c8b19fa4b
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-16 22:39:03 +0530

    [NIJAM] Add:Welecome To LinkedListStack.

---
+++ StackAndQueue_CSharp/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace StackAndQueue_CSharp
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ StackAndQueue_CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StackAndQueue_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To LinkedListStack");
+            
+        }
+    }
+}
